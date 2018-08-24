cb91600eb912d3cf77980134834cb9b792aac5da
Own Daghagheleh <>
2018-08-24 14:24:46 +1000

    change endpoint to 7.0.2

--- src/main/webapp/protected/databaseBackups/develop/servDataSource/api_oauth_tokens.sql
+++ src/main/webapp/protected/databaseBackups/develop/servDataSource/api_oauth_tokens.sql
@@ -1,1 +1,1 @@
-INSERT INTO api_oauth_tokens (accesstoken,clientid,expiresin,refreshtoken,granttype,localuser) VALUES ('a454e149dffc332ab1ffaa6e66955c66','LOCAL_USER','2018-08-23 15:49:22','ad31e29a1e39e228b6ec897b59b9af15','implicit','admin');
+INSERT INTO api_oauth_tokens (accesstoken,clientid,expiresin,refreshtoken,granttype,localuser) VALUES ('9afc6743eaf79450bc8f914decf3cc05','LOCAL_USER','2018-08-24 15:16:12','dc42efaafe73bc869596773b22495ec2','implicit','admin');
